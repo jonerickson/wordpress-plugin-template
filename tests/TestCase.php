94e985c863c81a8794a412688827a4eaed9ee01c
Jon Erickson <jon@deschutesdesigngroup.com>
2023-07-21 21:08:07 -0700

    Add php unit config

---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+abstract class TestCase extends BaseTestCase
+{
+    use WithFaker;
+}
