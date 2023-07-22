93ea62b14cf0b13a9a041593712965417414247d
Jon Erickson <jon@deschutesdesigngroup.com>
2023-07-21 18:13:34 -0700

    Remove duplicated files

--- yourplugin/resources/views/shortcodes/plugin.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.shortcode')
-
-@section('content')
-    <div class="font-bold text-indigo-600">
-        This is a Wordpress shortcode...
-    </div>
-@endsection
--- yourplugin/resources/views/welcome.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="p-16 flex justify-center items-center h-full">
-        <h1 class="text-4xl font-bold">Welcome to the Wordpress Plugin Template</h1>
-    </div>
-@endsection
