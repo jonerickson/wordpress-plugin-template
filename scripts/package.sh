b14964757c95211762f4fdf9e9aefbde1e313a50
Jon Erickson <jon@deschutesdesigngroup.com>
2023-08-24 14:21:25 -0700

    Update packager

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,13 +3,35 @@
 SOURCE_DIR="src"
 VENDOR_DIR="vendor"
 PLUGIN_FILE="yourplugin.php"
-ZIP_FILE="build/yourplugin.zip"
+ZIP_FILE="yourplugin.zip"
+
+# Delete existing build
+if [ -f "./build/$ZIP_FILE" ]; then
+  rm -r ./build/$ZIP_FILE
+fi
 
 # Move source over to build directory
-cp -r $SOURCE_DIR $VENDOR_DIR $PLUGIN_FILE build/
+if [ ! -f "./build/$PLUGIN_FILE" ]; then
+  cp -r $PLUGIN_FILE build/
+fi
+
+if [ ! -d "./build/$SOURCE_DIR" ]; then
+  cp -r $SOURCE_DIR build/
+fi
+
+if [ ! -d "./build/$VENDOR_DIR" ]; then
+  cp -r $VENDOR_DIR build/
+fi
+
+if [ ! -f "./build/composer.json" ]; then
+  cp -r composer.json build/
+fi
 
 # Change to build directory
 cd build
+
+# Update the autoloader to PHP-Scoper
+sed -i '' 's/\/autoload.php/\/scoper-autoload.php/g;' $PLUGIN_FILE
 
 ## Remove all files from the storage directories
 rm -rf src/storage/framework/cache/*
@@ -23,5 +45,6 @@ zip -r $ZIP_FILE * -x '*.log'
 rm -r $SOURCE_DIR
 rm -r $VENDOR_DIR
 rm -r $PLUGIN_FILE
+rm -r composer.json
 
 echo "Package complete."
