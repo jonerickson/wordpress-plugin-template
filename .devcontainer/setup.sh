08dbea2bd7acc795c4a8fe4085a65505f95ddddc
Jon Erickson <jon@deschutesdesigngroup.com>
2023-08-25 08:35:22 -0700

    Enable docker preview environment and fix wordpress setup routine to activate plugin at the end

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,7 +6,7 @@ sudo sed "s@.*DocumentRoot.*@\tDocumentRoot $PWD/wordpress@" .devcontainer/000-d
 update-rc.d apache2 defaults
 service apache2 start
 
-# WordPress Core Install
+# WordPress core install
 wp core download --locale=en_US --path=wordpress
 cd /workspace/wordpress
 wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=database
@@ -14,23 +14,22 @@ LINE_NUMBER=`grep -n -o 'stop editing!' wp-config.php | cut -d ':' -f 1`
 sed -i "${LINE_NUMBER}r ../.devcontainer/wp-config.txt" wp-config.php && sed -i -e "s/CODESPACE_NAME/$CODESPACE_NAME/g"  wp-config.php
 wp core install --url=https://$(CODESPACE_NAME) --title="Wordpress Plugin Template" --admin_user=wordpress --admin_password=wordpress --admin_email=mail@example.com
 
-# Enable Debug Mode
+# Enable debug mode
 wp config set WP_DEBUG true --raw
 
-# Manage Plugins
+# Manage plugins
 wp plugin delete akismet
 wp plugin install show-current-template --activate
 
-# Symlink Plugin and Activate
+# Symlink plugin
 mkdir -p "/workspace/wordpress/wp-content/plugins/yourplugin"
 cd /workspace/wordpress/wp-content/plugins/yourplugin
 ln -s /workspace/src
 ln -s /workspace/vendor
 ln -s /workspace/yourplugin.php
 cd /workspace/wordpress
-wp plugin activate yourplugin
 
-# Import Demo Content
+# Import demo content
 wp plugin install wordpress-importer --activate
 curl https://raw.githubusercontent.com/WPTT/theme-unit-test/master/themeunittestdata.wordpress.xml > demo-content.xml
 wp import demo-content.xml --authors=create
@@ -39,10 +38,13 @@ rm demo-content.xml
 # Xdebug
 echo xdebug.log_level=0 | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
 
-# Install Dependencies
+# Install dependencies
 cd /workspace
 yarn install
 composer install
 
-# Build App
+# Build app
 npm run build
+
+# Activate the plugin
+wp plugin activate yourplugin
